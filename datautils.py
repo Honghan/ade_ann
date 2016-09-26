73d22bc4cfb630f889eaf0be48f6ac19754ec0e4
honghan <honghan.wu@gmail.com>
2016-09-26 15:05:38 +0100

    fixed a few bugs

--- datautils.py
+++ datautils.py
@@ -43,21 +43,21 @@ def extract_cris_texts(out_path):
     ids = ''
     sqls = []
     for txt in txt_list:
-        if txt['src_table'] != table:
+        if txt.src_table != table:
             if table.strip() != '':
                 sqls.append(sql_template.format({'table': table, 'col':col, 'ids': ids}))
-            table = txt['src_table']
-            col = txt['src_col']
+            table = txt.src_table
+            col = txt.src_col
             ids = ''
-        ids += (',' if len(ids) > 0 else '') + "'" + txt['CN_Doc_ID'] + "'"
+        ids += (',' if len(ids) > 0 else '') + "'" + txt.CN_Doc_ID + "'"
     sqls.append(sql_template.format({'table': table, 'col': col, 'ids': ids}))
 
     q_data = ' union '.join(sqls)
     print 'query [\n%s\n]...' % q_data
-    rows = query_sqlsvr(q_txt_list, db_conn)
+    rows = query_sqlsvr(q_data, db_conn)
     for r in rows:
-        with codecs.open(join(out_path, r['fn']), 'w', encoding='utf-8') as wf:
-            wf.write(r['txt'])
+        with codecs.open(join(out_path, r.fn), 'w', encoding='utf-8') as wf:
+            wf.write(r.txt)
     print '{} files saved to {}'.format(len(rows), out_path)
 
 
